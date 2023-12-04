1fa7ee3abf24f0de993d073d30aaee847b084147
Alexander <Lisnevskiy14@gmail.com>
2023-12-04 11:50:50 +0300

    Added Note model

--- note/models.py
+++ note/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Note(models.Model):
+    title = models.CharField(max_length=255, verbose_name='название')
+    description = models.TextField(verbose_name='описание', **NULLABLE)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='дата создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='дата обновления')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'заметка'
+        verbose_name_plural = 'заметки'
