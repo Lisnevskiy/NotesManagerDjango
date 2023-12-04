d29f19e19998745de209dc0534ad0011c1527513
Alexander <Lisnevskiy14@gmail.com>
2023-12-04 12:25:38 +0300

    Added User tests

--- user/tests.py
+++ user/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class UserRegisterViewTests(TestCase):
+    def test_user_register_view(self):
+        response = self.client.get(reverse('user:register'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'user/register.html')
+
+
+class UserLoginViewTests(TestCase):
+    def test_user_login_view(self):
+        response = self.client.get(reverse('user:login'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'user/login.html')
+
+
+class UserLogoutViewTests(TestCase):
+    def test_user_logout_view(self):
+        response = self.client.get(reverse('user:logout'))
+        self.assertEqual(response.status_code, 302)
